837dd30d5771af1dc68b912537b45465eccd2db0
braedonmartin <braedonmartin25@gmail.com>
2022-07-15 15:29:40 -0400

    fixed the services cards

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Header from './../Common/Header';
 import Footer from './../Common/Footer';
-import Slider from './../Elements/Slider';
+import Slider from './../Elements/Slider2';
 import Specialization from './../Elements/Specialization';
-import About from './../Elements/About';
-import OurValue from './../Elements/OurValue';
-import OurMission from './../Elements/OurMission';
-import OurServices from './../Elements/OurServices';
-import Callus from './../Elements/Callus';
+import About from './../Elements/About3';
+import OurServices from './../Elements/OurServices2';
+import Team3 from './../Elements/Team3';
 import OurProject from './../Elements/OurProject';
-import Blogs from './../Elements/Blogs';
 import Testimonials from './../Elements/Testimonials';
 
 class Home extends React.Component {
@@ -19,12 +16,12 @@ class Home extends React.Component {
                 <Header />
                 <div className="page-content">
                     <Slider />
-                    <Specialization />
-                    <About />
+                    {/* <Specialization /> */}
                     <OurServices />
-                    <Callus />
-                    <OurProject />
-                    <Testimonials />
+                    <About />
+                    <Team3 />
+                    {/* <OurProject /> */}
+                    {/*<Testimonials />*/}
                 </div>
                 <Footer />
             </>
